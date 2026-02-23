c1458e65a8db64b5325d30c4efd4fcaa3326814f
AndriusSka <andrius.skamarakas@gmail.com>
2026-02-24 00:07:31 +0200

    added share recipe form to app.tsx

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navigation from "./components/navigation";
 import ComingSoon from "./components/comingsoon";
+import ShareRecipe from "./components/sharerecipe";
 
 function App() {
   return (
     <>
       <Navigation />
       <ComingSoon />
+      <ShareRecipe />
     </>
   );
 }
