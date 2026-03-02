e33ad5a3751733fd983c26444140e3c44f2f98a7
AndriusSka <andrius.skamarakas@gmail.com>
2026-03-02 13:06:15 +0200

    deleted comingsoon.tsx from app.tsx

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import Navigation from "./components/navigation";
-import ComingSoon from "./components/comingsoon";
 import ShareRecipe from "./components/sharerecipe";
 import Footer from "./components/footer";
 import RecipesPage from "./pages/recipepage";
