dbb1e77439e3b5bb0ec37f22a725396d0966242d
Gabrielius <gabrielius.paulikas@gmail.com>
2026-03-02 15:37:25 +0200

    Add global navigation menu and routes

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -3,12 +3,24 @@ import Navigation from "./components/navigation";
 import ShareRecipe from "./components/sharerecipe";
 import Footer from "./components/footer";
 import RecipesPage from "./pages/recipepage";
+import About from "./pages/About";
+import Account from "./pages/Account";
+import Settings from "./pages/Settings";
+
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Navigation />
-      <RecipesPage />
+      <Routes>
+        <Route path="/" element={<RecipesPage />} />
+        <Route path="/recipes" element={<Navigate to="/" replace />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+
       <ShareRecipe />
       <Footer />
     </>
---
+++ packages/frontend/src/pages/Settings.tsx
@@ -0,0 +1,3 @@
+export default function Settings() {
+  return <div className="px-6">Settings</div>;
+}
