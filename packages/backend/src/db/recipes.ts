64d47cc32f3f93eac227a091e2644798891a1c4e
Daviltew <daviltew@gmail.com>
2026-03-02 15:22:13 +0200

    Uploading images to database

--- packages/backend/src/db/recipes.ts
+++ packages/backend/src/db/recipes.ts
@@ -6,16 +6,24 @@ export async function createRecipe(input: {
   description: string;
   ingredients: string[];
   selectedTags: string[];
+  image?: string | null;
 }): Promise<Recipe> {
   const { rows } = await pool.query<Recipe>(
     `WITH new_recipe AS (
-            INSERT INTO recipes (title, description) VALUES ($1, $2) RETURNING id
+            INSERT INTO recipes (title, description, image) 
+            VALUES ($1, $2, $5) 
+            RETURNING id
         ),
         insert_ingredients AS (
-            INSERT INTO ingredients (recipe_id, info) SELECT new_recipe.id, unnest($3::text[]) FROM new_recipe
+            INSERT INTO ingredients (recipe_id, info) 
+            SELECT new_recipe.id, unnest($3::text[]) 
+            FROM new_recipe
         )
-        INSERT INTO recipe_tags (recipe_id, tag_id) SELECT new_recipe.id, tags.id FROM new_recipe JOIN tags ON tags.name = ANY($4::text[]);`,
-    [input.title, input.description, input.ingredients, input.selectedTags],
+        INSERT INTO recipe_tags (recipe_id, tag_id) 
+        SELECT new_recipe.id, tags.id 
+        FROM new_recipe 
+        JOIN tags ON tags.name = ANY($4::text[]);`,
+    [input.title, input.description, input.ingredients, input.selectedTags, input.image || null],
   );
   return rows[0];
 }
