119a025127401b14f9bf2d918bdf9a8e3bdcf3f1
AndriusSka <andrius.skamarakas@gmail.com>
2026-03-12 17:22:07 +0200

    Editted App.tsx

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -2,29 +2,29 @@ import "./App.css";
 import Navigation from "./components/navigation";
 import Footer from "./components/footer";
 import RecipesPage from "./pages/recipepage";
+import Home from "./pages/Home";
 import About from "./pages/About";
 import Account from "./pages/Account";
 import Settings from "./pages/Settings";
 import ShareRecipeForm from "./components/sharerecipe";
-
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <div className="min-h-screen flex flex-col">
-      <Navigation />
-      <main className="flex-1">
-      <Routes>
-        <Route path="/" element={<RecipesPage />} />
-        <Route path="/recipes" element={<Navigate to="/" replace />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/account" element={<Account />} />
-        <Route path="/settings" element={<Settings />} />
-      </Routes>
-      </main>
-      <ShareRecipeForm />
-      <Footer />
+        <Navigation />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/recipes" element={<RecipesPage />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </main>
+        <ShareRecipeForm />
+        <Footer />
       </div>
     </>
   );
