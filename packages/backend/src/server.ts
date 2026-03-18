790c444c3d2c9ff4b0afb4c26dbfee9b2ff7cc72
j5k0 <96486300+j5k0@users.noreply.github.com>
2026-03-18 21:31:16 +0200

    change SameSite cookie attribute to 'None'

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -125,7 +125,7 @@ loginRouter.post("/login", async (req: Request, res: Response) => {
     res.cookie("token", token, {
         httpOnly: true,
         secure: true,
-        sameSite: "strict"
+        sameSite: "None"
     });
     res.json({ message: "Logged in" });
 });
